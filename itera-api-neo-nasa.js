364e3d61a1ee884cb36d1791e94e6e528d54a699
Kleber da Silva Lyra <kleberlyra@gmail.com>
2024-10-25 09:44:59 -0400

    Update itera-api-neo-nasa.js
    atualizacao

--- itera-api-neo-nasa.js
+++ itera-api-neo-nasa.js
@@ -1,33 +1,26 @@
 //Itera o json da api da NASA e retorna todos os NEOs
 
 var output = [];
-
 var jsonData = JSON.parse(value).near_earth_objects;
 
-for (var obj in jsonData) {
-    
-    jsonData[obj].forEach(function(neo) {
-
-        output.push({
-            '{#NEO.ID}': neo.id,
+for (var o in jsonData) { 
+    jsonData[o].forEach(function(neo) { 
+        output.push({ 
+            '{#NEO.ID}' : neo.id,
+            '{#NEO.NAME}' : neo.name,
             '{#NEO.NEO_REFERENCE_ID}': neo.neo_reference_id,
-            '{#NEO.NAME}': neo.name,
             '{#NEO.NASA_JPL_URL}': neo.nasa_jpl_url,
             '{#NEO.ABSOLUTE_MAGNITUDE_H}': neo.absolute_magnitude_h,
-            '{#NEO.ESTIMATED_DIAMETER.METERS.ESTIMATED_DIAMETER_MIN}': neo.estimated_diameter.meters.estimated_diameter_min,
-            '{#NEO.ESTIMATED_DIAMETER.METERS.ESTIMATED_DIAMETER_MAX}': neo.estimated_diameter.meters.estimated_diameter_max,
+            '{#NEO.ESTIMATED_DIAMETER_MIN}': neo.estimated_diameter.meters.estimated_diameter_min,
+            '{#NEO.ESTIMATED_DIAMETER_MAX}': neo.estimated_diameter.meters.estimated_diameter_max,
             '{#NEO.IS_POTENTIALLY_HAZARDOUS_ASTEROID}': neo.is_potentially_hazardous_asteroid,
-            '{#NEO.CLOSE_APPROACH_DATA.CLOSE_APPROACH_DATE}': neo.close_approach_data.close_approach_date,
-            '{#NEO.CLOSE_APPROACH_DATE.CLOSE_APPROACH_DATE_FULL}': neo.close_approach_date[0].close_approach_date_full,
-            '{#NEO.CLOSE_APPROACH_DATE.RELATIVE_VELOCITY.KILOMETERS_PER_SECOND}': neo.close_approach_date.relative_velocity.kilometers_per_second,
-            '{#NEO.MISS_DISTANCE.ASTRONOMICAL}': neo.miss_distance.astronomical,
-            '{#NEO.MISS_DISTANCE.LUNAR}': neo.miss_distance.lunar,
-            '{#NEO.MISS_DISTANCE.KILOMETERS}': neo.miss_distance.kilometers,
-            '{#NEO.ORBITING_BODY}': neo.orbiting_body
-        });
-    
-    });
-
+            '{#NEO.CLOSE_APPROACH_DATE}': neo.close_approach_data[0].close_approach_date,
+            '{#NEO.EPOCH_DATE_CLOSE_APPROACH}': neo.close_approach_data[0].epoch_date_close_approach,
+            '{#NEO.MISS_DISTANCE}': neo.close_approach_data[0].miss_distance.kilometers,
+            '{#NEO.ORBITING_BODY}': neo.close_approach_data[0].orbiting_body,
+            '{#NEO.IS_SENTRY_OBJECT}': neo.is_sentry_object
+        }); 
+    }); 
 }
 
 return JSON.stringify(output);
